40e1f134f5eed562a87e3030fc4ddad807f01d33
jcolcombe <jcolcombe@outlook.com>
2021-02-11 18:37:08 +0000

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/jcolcombes-org/s3-bucket/aws"
   version = "1.15.0"
   acl    = "private"
-  bucket_prefix =var.prefix
+  name =${var.prefix}-S3Bucket
   # insert required variables here
 }
